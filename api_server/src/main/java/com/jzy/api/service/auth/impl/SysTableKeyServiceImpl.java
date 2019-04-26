32872de45f345ac791a7fc9a64f5bc01788e592e
liuhongchao <765161118@qq.com>
2019-04-26 09:12:18 +0800

    update:主键服务

--- api_server/src/main/java/com/jzy/api/service/auth/impl/SysTableKeyServiceImpl.java
+++ api_server/src/main/java/com/jzy/api/service/auth/impl/SysTableKeyServiceImpl.java
@@ -1,4 +1,3 @@
-/*
 package com.jzy.api.service.auth.impl;
 
 
@@ -16,7 +15,7 @@ import org.springframework.util.StringUtils;
 import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
 
-*/
+
 /**
  * <b>功能：</b>主键序列实现类<br>
  * <b>Copyright JZY</b>
@@ -25,7 +24,7 @@ import java.util.concurrent.TimeUnit;
  * <hr>
  * <li>v1.0&nbsp;&nbsp;&nbsp;&nbsp;20190425&nbsp;&nbsp;技术中心&nbsp;&nbsp;&nbsp;&nbsp;刘宏超$&nbsp;&nbsp;&nbsp;&nbsp;创建类</li>
  * </ul>
- *//*
+ */
 
 @Service
 public class SysTableKeyServiceImpl {
@@ -33,18 +32,10 @@ public class SysTableKeyServiceImpl {
     @Resource
     private RedissonClient redissonClient;
 
-
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    */
-/*
-     定义门槛阈值，当子增加值距  thresholdValue 取余小于阈值 1/10时候
-     数据库保证 自增值天花板值 + thresholdValue 更新
-     *//*
 
     private static final Long thresholdValue = 100L;
 
-*/
 /**
  * <b>功能：</b>获取主键<br>
  * <b>Copyright JZY</b>
@@ -53,12 +44,12 @@ public class SysTableKeyServiceImpl {
  * <hr>
  * <li>v1.0&nbsp;&nbsp;&nbsp;&nbsp;20190425&nbsp;&nbsp;技术中心&nbsp;&nbsp;&nbsp;&nbsp;刘宏超$&nbsp;&nbsp;&nbsp;&nbsp;创建类</li>
  * </ul>
- *//*
+ */
 
     public long newkey(String tableName, String columnName, int centerId) {
 
-        final String lockStr = "PK:".concat(tableName).concat(columnName);
-        RLock lock = null;// 店铺登录加上同步锁，防止并发请求数据异常
+        final String lockStr = "PK_Lock:".concat(tableName).concat(columnName);
+        RLock lock = null;// 加上同步锁，防止并发请求数据异常
         try {
             lock = redissonClient.getLock(lockStr);
             lock.lock(10, TimeUnit.SECONDS);
@@ -75,7 +66,7 @@ public class SysTableKeyServiceImpl {
         }
     }
 
-*/
+
 /**
  * <b>功能：</b>获取新主键- 从redis中自增<br>
  * <b>Copyright JZY</b>
@@ -84,7 +75,7 @@ public class SysTableKeyServiceImpl {
  * <hr>
  * <li>v1.0&nbsp;&nbsp;&nbsp;&nbsp;20190425&nbsp;&nbsp;技术中心&nbsp;&nbsp;&nbsp;&nbsp;刘宏超$&nbsp;&nbsp;&nbsp;&nbsp;创建类</li>
  * </ul>
- *//*
+ */
 
     public Long getAutoSequenceCode(Integer centerId, String tableName, String columnName) {
 
@@ -93,7 +84,7 @@ public class SysTableKeyServiceImpl {
         return result;
     }
 
-    */
+
 /**
      * <b>功能：</b>获取新主键- 从redis中自增<br>
      * <b>Copyright JZY</b>
@@ -102,11 +93,11 @@ public class SysTableKeyServiceImpl {
      * <hr>
      * <li>v1.0&nbsp;&nbsp;&nbsp;&nbsp;20190425&nbsp;&nbsp;技术中心&nbsp;&nbsp;&nbsp;&nbsp;刘宏超$&nbsp;&nbsp;&nbsp;&nbsp;创建类</li>
      * </ul>
-     *//*
+     */
 
     private Long increasing(String tableName, String columnName, Integer centerId) {
         Long curVal = 1L;
-        final String tableKey = "SysPK:".concat(tableName).concat(columnName);
+        final String tableKey = "PK_Sys:".concat(tableName).concat(columnName);
         RAtomicLong pkIndex = redissonClient.getAtomicLong(tableKey);
 
         if (pkIndex.isExists()) {
@@ -118,4 +109,4 @@ public class SysTableKeyServiceImpl {
     }
 
 }
-*/
+
