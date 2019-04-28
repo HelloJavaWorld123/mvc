40b8eda8abcfee8927f5f57b12590fc928ece658
dengchong <dc_struggle@163.com>
2019-04-28 09:58:18 +0800

    update：交易记录接口定义

--- api_server/src/main/java/com/jzy/api/service/biz/TradeRecordService.java
+++ api_server/src/main/java/com/jzy/api/service/biz/TradeRecordService.java
@@ -14,6 +14,54 @@ import com.jzy.framework.service.GenericService;
  */
 public interface TradeRecordService extends GenericService<TradeRecord> {
 
+    /**
+     * <b>功能描述：</b>更新交易记录<br>
+     * <b>修订记录：</b><br>
+     * <li>20190428&nbsp;&nbsp;|&nbsp;&nbsp;邓冲&nbsp;&nbsp;|&nbsp;&nbsp;创建方法</li><br>
+     *
+     * @param markId 第三方支付订单号
+     * @param status [refushed、failed、passed、noresponse]
+     * @param bgRespData 异步通知数据
+     * @param id 交易唯一标识trade_record_id
+     * @param oldStatus
+     */
+    boolean updateBgRespByIdStatus(String markId, String status, String bgRespData, String id, String oldStatus);
 
+    /**
+     * 更新交易记录
+     * <p>
+     * 1:(微信)退款通知
+     *
+     * @param markid     第三方支付订单号
+     * @param status     [refushed、failed、passed、noresponse]
+     * @param bgRespData 异步通知数据
+     * @param operator   交易唯一标识trade_record_operator
+     * @return
+     */
+    boolean updateBgRespByOperatorStatus(String markid, String status, String bgRespData, String operator, String oldStatus);
 
+    /**
+     * <b>功能描述：</b>更新交易记录<br>
+     * <b>修订记录：</b><br>
+     * <li>20190428&nbsp;&nbsp;|&nbsp;&nbsp;邓冲&nbsp;&nbsp;|&nbsp;&nbsp;创建方法</li><br>
+     *
+     * 1:(支付宝)支付完成回调
+     * @param markid
+     * @param status
+     * @param respData
+     * @param operator
+     * @param oldStatus
+     */
+    boolean updateRespByOperatorStatus(String markid, String status, String respData, String operator, String oldStatus);
+
+    /**
+     * <b>功能描述：</b>查询<br>
+     * <b>修订记录：</b><br>
+     * <li>20190428&nbsp;&nbsp;|&nbsp;&nbsp;邓冲&nbsp;&nbsp;|&nbsp;&nbsp;创建方法</li><br>
+     *
+     * @param operator 操作订单号,操作人
+     * @param type 交易类型(支付:pay、退款:refund)
+     * @param status 交易状态
+     */
+    TradeRecord queryOperator(String operator, String type , String status);
 }
--- api_server/src/main/java/com/jzy/api/service/biz/impl/TradeRecordServiceImpl.java
+++ api_server/src/main/java/com/jzy/api/service/biz/impl/TradeRecordServiceImpl.java
@@ -24,7 +24,68 @@ public class TradeRecordServiceImpl extends GenericServiceImpl<TradeRecord> impl
     @Resource
     private TradeRecordMapper tradeRecordMapper;
 
+    /**
+     * <b>功能描述：</b>更新交易记录<br>
+     * <b>修订记录：</b><br>
+     * <li>20190428&nbsp;&nbsp;|&nbsp;&nbsp;邓冲&nbsp;&nbsp;|&nbsp;&nbsp;创建方法</li><br>
+     *
+     * @param markId 第三方支付订单号
+     * @param status [refushed、failed、passed、noresponse]
+     * @param bgRespData 异步通知数据
+     * @param id 交易唯一标识trade_record_id
+     * @param oldStatus
+     */
+    @Override
+    public boolean updateBgRespByIdStatus(String markId, String status, String bgRespData, String id, String oldStatus) {
+        return false;
+    }
 
+    /**
+     * 更新交易记录
+     * <p>
+     * 1:(微信)退款通知
+     *
+     * @param markid     第三方支付订单号
+     * @param status     [refushed、failed、passed、noresponse]
+     * @param bgRespData 异步通知数据
+     * @param operator   交易唯一标识trade_record_operator
+     * @return
+     */
+    @Override
+    public boolean updateBgRespByOperatorStatus(String markid, String status, String bgRespData, String operator, String oldStatus) {
+        return false;
+    }
+
+    /**
+     * <b>功能描述：</b>更新交易记录<br>
+     * <b>修订记录：</b><br>
+     * <li>20190428&nbsp;&nbsp;|&nbsp;&nbsp;邓冲&nbsp;&nbsp;|&nbsp;&nbsp;创建方法</li><br>
+     *
+     * 1:(支付宝)支付完成回调
+     * @param markid
+     * @param status
+     * @param respData
+     * @param operator
+     * @param oldStatus
+     */
+    @Override
+    public boolean updateRespByOperatorStatus(String markid, String status, String respData, String operator, String oldStatus) {
+        return false;
+    }
+
+    /**
+     * <b>功能描述：</b>查询<br>
+     * <b>修订记录：</b><br>
+     * <li>20190428&nbsp;&nbsp;|&nbsp;&nbsp;邓冲&nbsp;&nbsp;|&nbsp;&nbsp;创建方法</li><br>
+     *
+     * @param operator 操作订单号,操作人
+     * @param type 交易类型(支付:pay、退款:refund)
+     * @param status 交易状态
+     */
+    @Override
+    public TradeRecord queryOperator(String operator, String type, String status) {
+        return null;
+    }
 
     @Override
     protected GenericMapper<TradeRecord> getGenericMapper() {
