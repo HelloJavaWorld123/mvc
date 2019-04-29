5e0559d1360928ba0d48990ef92a8b033e21c84d
dengchong <dc_struggle@163.com>
2019-04-29 11:17:27 +0800

    update：代码迁移

--- api_server/src/main/java/com/jzy/api/dao/biz/OrderMapper.java
+++ api_server/src/main/java/com/jzy/api/dao/biz/OrderMapper.java
@@ -2,7 +2,10 @@ package com.jzy.api.dao.biz;
 
 import com.jzy.api.model.biz.Order;
 import com.jzy.framework.dao.GenericMapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.security.access.method.P;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -49,5 +52,50 @@ public interface OrderMapper extends GenericMapper<Order> {
      * @param id 订单id
      */
     int updateOrderDelFlag(Long id);
+
+    /**
+     * <b>功能描述：</b>更新订单状态<br>
+     * <b>修订记录：</b><br>
+     * <li>20190429&nbsp;&nbsp;|&nbsp;&nbsp;邓冲&nbsp;&nbsp;|&nbsp;&nbsp;创建方法</li><br>
+     */
+    int updateStatus(@Param("id") String id, @Param("status") Integer status);
+
+    /**
+     * <b>功能描述：</b>更新订单状态<br>
+     * <b>修订记录：</b><br>
+     * <li>20190429&nbsp;&nbsp;|&nbsp;&nbsp;邓冲&nbsp;&nbsp;|&nbsp;&nbsp;创建方法</li><br>
+     */
+    int updateTradeStatus(@Param("id") String id, @Param("tradeStatus") Integer tradeStatus);
+
+    /**
+     * <b>功能描述：</b>更新支付状态<br>
+     * <b>修订记录：</b><br>
+     * <li>20190419&nbsp;&nbsp;|&nbsp;&nbsp;邓冲&nbsp;&nbsp;|&nbsp;&nbsp;创建方法</li><br>
+     *
+     * @param id 订单id
+     * @param status 订单状态0待支付,1充值中,2充值成功,3充值失败,4充值关闭
+     * @param supStatus SUP返回的状态 sup状态，0未提交1已提交2成功3失败
+     * @param tradeStatus 支付状态
+     */
+    int updateStatusTradeStatusSupStatus(String id, Integer status, String tradeStatus, Integer supStatus);
+
+    int updateStatusTrade(String id, Integer status, String tradeCode, BigDecimal tradeFee, String tradeStatus);
+
+    int updateStatusSupStatusTrade(String id, Integer status, Integer supStatus, String tradeCode, BigDecimal tradeFee, String tradeStatus);
+
+    /**
+     * <b>功能描述：</b>更新订单的支付方式，流水号，交易状态<br>
+     * <b>修订记录：</b><br>
+     * <li>20190419&nbsp;&nbsp;|&nbsp;&nbsp;邓冲&nbsp;&nbsp;|&nbsp;&nbsp;创建方法</li><br>
+     *
+     * @param id 订单id
+     * @param status 订单状态；0待支付, 1充值中,   2充值成功,  3充值失败,  4充值关闭
+     * @param tradeMethod 支付方式；0：微信；1：支付宝
+     * @param outTradeNo 流水号
+     */
+    int updateStatusTradeMethod(String id, Integer status, Integer tradeMethod, String outTradeNo);
+
+
+
 
 }
