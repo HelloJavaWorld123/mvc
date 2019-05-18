dd859b2ce25057762d48b8facb4b7fe862b393bf
dengchong <dc_struggle@163.com>
2019-05-18 16:55:21 +0800

    update：验签失败sup返回error

--- api_server/src/main/java/com/jzy/api/service/biz/impl/SupServiceImpl.java
+++ api_server/src/main/java/com/jzy/api/service/biz/impl/SupServiceImpl.java
@@ -158,7 +158,7 @@ public class SupServiceImpl extends GenericServiceImpl<SupRecord> implements Sup
         if (!md5Sign.equals(sign)) {
             log.debug("SUP订单异步通知验签失败,本地签名md5Sign=".concat(md5Sign).concat(",请求签名sign=").concat(sign));
             orderService.updateStatusTradeStatusSupStatus(order.getOrderId(), 3, Order.TradeStatusConst.REFUND_SICCESS,3);
-            response.getWriter().write("<receive>ok</receive>");
+            response.getWriter().write("<receive>error</receive>");
             return;
         }
         // 获取SUP充值记录
