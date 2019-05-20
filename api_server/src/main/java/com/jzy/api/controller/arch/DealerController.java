68aa96f7199054556dd8cb4fd7d924cecbaa028e
唐永刚 <875817562@qq.com>
2019-05-20 14:24:40 +0800

    add:渠道商商品面值信息增量更新

--- api_server/src/main/java/com/jzy/api/controller/arch/DealerController.java
+++ api_server/src/main/java/com/jzy/api/controller/arch/DealerController.java
@@ -16,7 +16,6 @@ import com.jzy.framework.result.ApiResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
-import org.springframework.data.annotation.Id;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
