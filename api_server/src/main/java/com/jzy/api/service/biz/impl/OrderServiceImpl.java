9385dcf1e505a46a8f1576887b72caa11a75155e
“liuchilei” <liuchilei@jiazhiyi.com>
2019-05-27 15:29:11 +0800

    bug 902 API后台订单列表，选择当日数据，导出Excel，点击下载，查看Excel数据

--- api_server/src/main/java/com/jzy/api/service/biz/impl/OrderServiceImpl.java
+++ api_server/src/main/java/com/jzy/api/service/biz/impl/OrderServiceImpl.java
@@ -461,7 +461,8 @@ public class OrderServiceImpl extends GenericServiceImpl<Order> implements Order
      */
     @Override
     public List<Order> queryExcelExportBackOrderList(BackOrderCnd backOrderCnd) {
-        PageHelper.startPage(backOrderCnd.getPage(), backOrderCnd.getLimit(), false);
+        //订单导出不分页 st20190527 解决902bug
+        //PageHelper.startPage(backOrderCnd.getPage(), backOrderCnd.getLimit(), false);
         return orderMapper.queryBackOrderList(backOrderCnd.getStartDate(), backOrderCnd.getEndDate(),
                 backOrderCnd.getSupStatus(), backOrderCnd.getStatus(), backOrderCnd.getKey(), backOrderCnd.getDealerId(),
                 getDealerId());
