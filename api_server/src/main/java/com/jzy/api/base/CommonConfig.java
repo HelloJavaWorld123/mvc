6745c35e95404b672a6713d4a8ec1314589ea177
renxiaokuan <1806146700@qq.com>
2019-05-31 14:03:49 +0800

    暂时去除swagger的相关配置

--- api_server/src/main/java/com/jzy/api/base/CommonConfig.java
+++ api_server/src/main/java/com/jzy/api/base/CommonConfig.java
@@ -3,6 +3,7 @@ package com.jzy.api.base;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.validation.Validator;
@@ -14,7 +15,9 @@ import javax.validation.Validator;
  * Desc:
  **/
 @Configuration
+@EnableWebMvc
 public class CommonConfig extends WebMvcConfigurerAdapter {
+
 
 	@Bean
 	public Validator validator(){
--- api_server/src/main/java/com/jzy/api/controller/auth/AuthController.java
+++ api_server/src/main/java/com/jzy/api/controller/auth/AuthController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
